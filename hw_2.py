7463f1be4c8fea6e220367e4d4b1824b68aa51f9
Anoshkin <56596356+Anoshkin@users.noreply.github.com>
2020-05-18 00:33:04 +0300

    Create hw_2.py

---
+++ hw_2.py
@@ -0,0 +1,13 @@
+a = {'20','40' ,'60'}
+b = {'30','40','50'}
+c = {'20','50','70'}
+
+set.union(a,b)
+
+set.intersection(a,b)
+
+set.difference(c,b)
+
+set.symmetric_difference(a,c)
+
+set.intersection(set.difference(set.union(set.difference(c,b),a),c),b)
